91a91e0f9baf57fd43246f34b413f4b6f760b48e
aawezk456 <aawezk456@gmail.com>
2020-10-01 22:23:09 +0530

    Update : Home Page

---
+++ lib/constant/data_json.dart
@@ -0,0 +1,36 @@
+List items = [
+  {
+    "videoUrl" : "assets/videos/video_1.mp4",
+    "name" : "Aawez Khan",
+    "caption" : "Morning, everyone!!",
+    "songName" : "original sound - Aawez Khan",
+    "profileImg" : "https://media-exp1.licdn.com/dms/image/C5103AQE8QK8N3UVzaA/profile-displayphoto-shrink_200_200/0?e=1606953600&v=beta&t=_rAAfLcQ_wE8wf51U_FwxQBUPaVJN0EwOi_1D62UQzA",
+    "likes" : "1.5M",
+    "comments" : "18.9K",
+    "shares" : "80K",
+    "albumImg" : "https://images.unsplash.com/photo-1502982899975-893c9cf39028?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
+  },
+  {
+    "videoUrl" : "assets/videos/video_2.mp4",
+    "name" : "Dara Chamroeun",
+    "caption" : "Oops 🙊 #fyp #cambodiatiktok",
+    "songName" : "original sound - 💛💛Khantana 🌟",
+    "profileImg" : "https://p16-tiktokcdn-com.akamaized.net/aweme/720x720/tiktok-obj/ba13e655825553a46b1913705e3a8617.jpeg",
+    "likes" : "4.4K",
+    "comments" : "5.2K",
+    "shares" : "100",
+    "albumImg" : "https://images.unsplash.com/photo-1462804512123-465343c607ee?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=668&q=80"
+  },
+  {
+    "videoUrl" : "assets/videos/video_3.mp4",
+    "name" : "9999womenfashion",
+    "caption" : "#블루모드",
+    "songName" : "original sound - 🖤Khün MÄk🇰🇭",
+    "profileImg" : "https://p16-tiktokcdn-com.akamaized.net/aweme/720x720/tiktok-obj/1664576339652610.jpeg",
+    "likes" : "100K",
+    "comments" : "10K",
+    "shares" : "8.5K",
+    "albumImg" : "https://images.unsplash.com/photo-1457732815361-daa98277e9c8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80"
+  },
+ 
+];
